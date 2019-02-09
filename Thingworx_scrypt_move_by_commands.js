5e61760ba6e46c6a19bbac249f75fe1cea72b994
Ivan <glebov.vanya@list.ru>
2019-02-09 23:06:08 +0300

    added sourse zone 3 and 4

--- Thingworx_scrypt_move_by_commands.js
+++ Thingworx_scrypt_move_by_commands.js
@@ -1,15 +1,23 @@
 switch(me.counter){
-	//sourse sourse zone 1
-	case 1: me.PosX = 160; me.PosY = 160; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break;
-	//sourse sourse zone 2
-	case 2: me.PosX = 205; me.PosY = 175; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break; 
-	case 3: me.PosX = 
-	case 4:
+	//sourse zone 1
+	case 1: me.PosX = 160; me.PosY = 160; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; me.LastCase = 1; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break;
+	//sourse zone 2
+	case 2: me.PosX = 205; me.PosY = 175; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; me.LastCase = 2; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break; 
+	//sourse zone 3 
+	case 3: me.PosX = 167; me.PosY = 240; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; me.LastCase = 3; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break;
+	//sourse zone 4 
+	case 4: me.PosX = 198; me.PosY = 250; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; me.LastCase = 4; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break;
+	//push zone 1
 	case 5:
+	//push zone 2
 	case 6:
+	//push zone 3
 	case 7:
+	//push zone 4
 	case 8:
+	//take object
 	case 9:
+	//drop object
 	case 10:
 	default:
 }
