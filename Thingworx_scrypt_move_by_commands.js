9ee1a3952483ec21951c589a024c75113e72610a
Ivan <glebov.vanya@list.ru>
2019-02-09 22:33:00 +0300

    Added source zone 1

--- Thingworx_scrypt_move_by_commands.js
+++ Thingworx_scrypt_move_by_commands.js
@@ -1,6 +1,15 @@
 switch(me.counter){
-	case : 
-	case :
-	case :
+	//sourse zone 1
+	case 1: me.PosX = 160; me.PosY = 160; me.LightColorCode = 1; me.Grip = 0; me.counter = 9; result = {"X1" : me.PosX, "Y1" : me.PosY, "L1" : me.LightColorCode, "G1" : me.Grip}; break;
+	//sourse zone 2
+	case 2: me.PosX = 70
+	case 3: me.PosX = 
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
 	default:
 }
