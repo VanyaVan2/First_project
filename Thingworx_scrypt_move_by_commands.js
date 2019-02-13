e32269e20e39b8a0728d856abe5091a2504214a6
Ivan <glebov.vanya@list.ru>
2019-02-13 16:24:28 +0300

    Added color changing after work

--- Thingworx_scrypt_move_by_commands.js
+++ Thingworx_scrypt_move_by_commands.js
@@ -48,6 +48,7 @@ switch(me.counter){
 			case 8: me.counter = 3; break;
 		}
 		//me.CurrentCommandNumber = me.CurrentCommandNumber + 1;
+		me.LightColorCode = 2;
 		result = {"X1" : me.PosX, "Y1" : me.PosY, "T1" : me.RotT, "G1" : me.Grip, "L1" : me.LightColorCode, "N" : me.CurrentCommandNumber}; 
 		me.JSON_read.AddRow({N : me.Row, JsonInfo : result, JsonTextInfo : result, SendTime : me.TimeNow}); me.Row ++;
 		break;
@@ -64,6 +65,7 @@ switch(me.counter){
 			case 8: me.counter = 3; break;
 		}
 		//me.CurrentCommandNumber = me.CurrentCommandNumber + 1;
+		me.LightColorCode = 2;
 		result = {"X1" : me.PosX, "Y1" : me.PosY, "T1" : me.RotT, "G1" : me.Grip, "L1" : me.LightColorCode, "N" : me.CurrentCommandNumber};
 		me.JSON_read.AddRow({N : me.Row, JsonInfo : result, JsonTextInfo : result, SendTime : me.TimeNow}); me.Row ++;
 		break;
